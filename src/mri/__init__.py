e56577c5becbda94b2e95b56a7e8e45f707c41b4
avargs <davidvargas005@hotmail.com>
2023-02-12 16:23:48 +0100

    Integrate code from google colab notebook

--- src/mri/__init__.py
+++ src/mri/__init__.py
@@ -1,6 +1,65 @@
 import sys
+import json
 
-if sys.version_info[:2] >= (3, 8):
+import numpy as np
+import nibabel as nib
+
+
+def load_matrix_from_nifti_file(nifti_file_path):
+    nifti_data = nib.load(nifti_file_path)
+    nifti_matrix = np.array(nifti_data.dataobj, dtype='float')
+
+    return nifti_matrix
+
+
+def load_data(prefix):
+    # Store 3d images in a list and reading the json files to create a EchoTime vector
+    suffixes = ['0224-1', '0448-2', '0672-3', '0896-4', '1120-5', '1344-6', '1568-7', '1792-8']
+
+    list_mri = []
+    te_matrix_list = np.empty([8, 1], dtype=float)
+
+    for i in range(len(suffixes)):
+        path_nii = prefix + suffixes[i] + '.nii'
+        img = nib.load(path_nii)
+        data_nii = np.array(img.dataobj)
+        list_mri.append(data_nii)
+
+        path_json = prefix+suffixes[i]+'.json'
+        file = open(path_json)
+        data_json = json.load(file)
+
+        for j in data_json['acqpar']:
+            te_matrix_list[i, 0] = j['EchoTime']
+
+    return (te_matrix_list, list_mri)
+
+
+def ordinary_least_squares(voxel_signal_matrix, te_matrix_list):
+    voxel_signal_matrix[voxel_signal_matrix == 0] = 1
+    voxel_signal_matrix = np.log(voxel_signal_matrix)
+
+    echo_time_vector = te_matrix_list.reshape([1, 8])
+
+    dx = echo_time_vector - np.mean(echo_time_vector)
+    dy = voxel_signal_matrix - np.mean(voxel_signal_matrix)
+
+    # Transform the 4D array into a 2D matrix
+    dy = dy.reshape((8, np.product(dy.shape[1:])))
+
+    numerator = np.matmul(dx, dy)
+    numerator = np.sum(numerator, axis=0)
+    numerator = numerator.reshape(voxel_signal_matrix.shape[1:])
+
+    dx_sq = np.sum((echo_time_vector - np.mean(echo_time_vector)) ** 2)
+    denominator = np.sum(dx_sq)
+
+    result = -numerator / denominator
+
+    return result
+
+
+if sys.version_info[:2] >= (4, 0):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
     from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 else:
---
+++ src/mri/__main__.py
@@ -0,0 +1,67 @@
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mri import load_data, load_matrix_from_nifti_file, ordinary_least_squares
+
+
+def get_config(args):
+    config = dict()
+    config["input_data_folder"] = args[1]
+    config["input_data_prefix"] = args[2]
+    config["reference_relaxation_rate_map_path"] = args[3]
+    
+    return config
+
+
+def show_slice(relaxation_rate_map_matrix, slice_index=None):
+    if slice_index is None:
+        slice_index = relaxation_rate_map_matrix.shape[-1] // 2
+
+    plt.figure()
+    plt.imshow(relaxation_rate_map_matrix[:, :, slice_index])
+
+
+def print_stats(relaxation_rate_map_matrix, name=""):
+    print('Mean relaxation rate:', np.mean(relaxation_rate_map_matrix))
+    print('Median relaxation rate:', np.median(relaxation_rate_map_matrix))
+    print('Min relaxation rate:', np.min(relaxation_rate_map_matrix))
+    print('Max relaxation rate:', np.max(relaxation_rate_map_matrix))
+
+
+def main():
+    # Get config parameters from the program's argument
+    config = get_config(sys.argv)
+
+    # Load images from nii1 files and store them into a 4D matrix hmri_matrix_list[TE, r] where:
+    # - the first dimension (TE) indexes the successively sampled 3D hMRI matrices.
+    # - the 3D hMRI matrix map 3d positions (r) to space-sampled intensities.
+    #
+    # The echo time (TE) for each matrix is loaded as well and stored into a the vector te_matrix_list[TE] where:
+    # - the first dimension (TE) indexes the successive negated sample times as 3d matrices
+    (te_matrix_list, hmri_matrix_list) = load_data(config["input_data_folder"] + config["input_data_prefix"])
+
+    # Compute the ordinary least square solution
+    relaxation_rate_map_matrix = ordinary_least_squares(hmri_matrix_list, te_matrix_list)
+
+    # Scale back the relaxation map data to the range of the reference relaxation map
+    relaxation_rate_map_matrix *= 1000.0
+
+    # Show and print the stats for the produced relaxation rate map
+    show_slice(relaxation_rate_map_matrix)
+    print_stats(relaxation_rate_map_matrix)
+    print()
+
+    # Load the reference relaxation map
+    reference_relaxation_rate_map_matrix = load_matrix_from_nifti_file(config["reference_relaxation_rate_map_path"])
+
+    # Show and print the stats of the reference relaxation map
+    show_slice(reference_relaxation_rate_map_matrix)
+    print_stats(reference_relaxation_rate_map_matrix)
+    print()
+
+    plt.show()
+
+
+main()
