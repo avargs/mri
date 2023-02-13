dfb5320144e6078db4541b7302e1c278a72955f7
avargs <davidvargas005@hotmail.com>
2023-02-13 22:15:08 +0100

    Ensure a functioning dev-version through the integrate.sh script

---
+++ integrate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git push
+pip uninstall mri
+
+
+pip install "git+https://github.com/avargs/mri" && \
+\
+py -m mri \
+   ../data/t1w_mfc_3dflash_v1i_R4_0015/ \
+   anon_s2018-02-28_18-26-190921-00001 \
+   "../data/anon_s2018-02-28_18-26-185345-00001-00224-1_RFSC_R2s_OLS.nii"
